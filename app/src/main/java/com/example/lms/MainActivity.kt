ddcdeb4fe74b7ce45bbe451a690f0a42c224d3b6
AdliFauzan <fauzan.adli92@gmail.com>
2024-05-14 00:19:00 +0700

    Testing Dev LMS FauzanMobile

---
+++ app/src/main/java/com/example/lms/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.lms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.webViewClient = WebViewClient()
+
+        // this will load the url of the website
+        webView.loadUrl("https://lmsdev.menara-agung.com/")
+
+        // this will enable the javascript settings, it can also allow xss vulnerabilities
+        webView.settings.javaScriptEnabled = true
+
+        // if you want to enable zoom feature
+        webView.settings.setSupportZoom(true)
+    }
+    override fun onBackPressed() {
+        // if your webview can go back it will go back
+        if (webView.canGoBack())
+            webView.goBack()
+        // if your webview cannot go back
+        // it will exit the application
+        else
+            super.onBackPressed()
+    }
+}
